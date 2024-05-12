9b34fd61f114ee1afba963f1e4a07ae04a6a6cb0
nek1kek <vip.danya.pavlov.2017@mail.ru>
2024-05-12 14:49:03 +0300

    done

--- 9sem/main.py
+++ 9sem/main.py
@@ -1,38 +1,84 @@
 import os
 
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy import signal
+import matplotlib.pyplot as plt
+import itertools
+
 from scipy.io import wavfile
-from scipy.signal import savgol_filter
+from scipy import signal
+from tqdm import tqdm
+
+dpi = 1000
 
 
-def plot_and_save_spectrogram(samples, sample_rate, filename):
-    plt.figure(figsize=(10, 4))
-    f, t, Sxx = signal.spectrogram(samples, fs=sample_rate, window='hann', nperseg=512, noverlap=256)
-    plt.pcolormesh(t, f, 10 * np.log10(Sxx + 1e-10), shading='gouraud')
-    plt.ylabel('Частота [Hz]')
-    plt.xlabel('Время [s]')
+def spectrogram(samples, sample_rate, filename):
+    freq, t, spec = signal.spectrogram(samples, sample_rate, scaling='spectrum', window=('hann'))
+
+    spec = np.log10(spec + 1)
+    plt.pcolormesh(t, freq, spec, shading='gouraud', vmin=spec.min(), vmax=spec.max())
+    plt.ylabel('Частота [Гц]')
+    plt.xlabel('Время [с]')
+
     plt.savefig(filename)
-    plt.clf()
+
+    return freq, t, spec
+
+
+def make_butter_filter(sample_rate, data, output_dir):
+    b, a = signal.butter(10, 0.1, btype='lowpass')
+    filtered_signal = signal.filtfilt(b, a, data)
+    wavfile.write(os.path.join(output_dir, 'butter.wav'), sample_rate, filtered_signal.astype(np.int16))
+    spectrogram(filtered_signal, sample_rate, os.path.join(output_dir, 'butter.png'))
+
+
+def make_savgol_filter(sample_rate, data, output_dir):
+    denoised_savgol = signal.savgol_filter(data, 75, 5)
+    wavfile.write(os.path.join(output_dir, 'savgol.wav'), sample_rate, denoised_savgol.astype(np.int16))
+    spectrogram(denoised_savgol, sample_rate, os.path.join(output_dir, 'savgol.png'))
+
+
+def get_peaks(sample_rate, data, output_dir):
+    peaks = set()
+    delta_t = 0.1
+    delta_freq = 50
+
+    freq, t, spec = spectrogram(data, sample_rate, os.path.join(output_dir, 'input.png'))
+
+    for i in tqdm(range(len(freq)), desc='get_peaks'):
+        for j in range(len(t)):
+            index_t = np.asarray(abs(t - t[j]) < delta_t).nonzero()[0]
+            index_freq = np.asarray(abs(freq - freq[i]) < delta_freq).nonzero()[0]
+            indexes = np.array([x for x in itertools.product(index_freq, index_t)])
+            flag = True
+            for a, b in indexes:
+                if spec[i, j] <= spec[a, b] and i != a and i != b:
+                    flag = False
+                    break
+
+            if flag:
+                peaks.add(t[j])
+
+    with open(os.path.join(output_dir, 'peaks.txt'), 'w') as f:
+        f.write(str(len(peaks)))
+        f.write('\n')
+        f.write(str(peaks))
 
 
 def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    input_file = os.path.join(current_dir, 'input', 'cover.wav')
+    input_path = os.path.join(current_dir, 'input')
     output_path = os.path.join(current_dir, 'output')
     os.makedirs(output_path, exist_ok=True)
 
-    sample_rate, samples = wavfile.read(input_file)
+    sample_rate, data = wavfile.read(os.path.join(input_path, 'cover.wav'))
 
-    # Генерация и сохранение исходной спектрограммы
-    plot_and_save_spectrogram(samples, sample_rate, f'{output_path}/original_spectrogram.png')
+    make_butter_filter(sample_rate, data, output_path)
+    print('done butter filter')
 
-    # Применение шумопонижения и сохранение результата
-    filtered_samples = savgol_filter(samples, 100, 3)
-    plot_and_save_spectrogram(filtered_samples, sample_rate, f'{output_path}/filtered_spectrogram.png')
+    make_savgol_filter(sample_rate, data, output_path)
+    print('done savgol filter')
 
-    wavfile.write(f'{output_path}/filtered_audio.wav', sample_rate, filtered_samples.astype(np.int16))
+    get_peaks(sample_rate, data, output_path)
 
 
 if __name__ == '__main__':
